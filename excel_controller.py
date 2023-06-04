ff88afd6a7d90f1366ce034cad51e7485ef9b094
MasterGeek <masterivanic@gmail.com>
2023-06-04 22:09:23 +0200

    fix: metadata export as excel file

--- excel_controller.py
+++ excel_controller.py
@@ -9,7 +9,9 @@ from metadata import XlsMeta
 
 
 class ExcelUtils(ABC):
-    """define here"""
+    """define here https://www.youtube.com/watch?v=mJ1N7-HyH1A
+    https://www.youtube.com/watch?v=1atJmWitF74
+    """
 
     @abstractmethod
     def get_file_metadata(self, dir_path: str) -> MetaData:
@@ -41,11 +43,20 @@ class ExcelFileHandler(ExcelUtils):
     def __init__(self, file_dir: str = None) -> None:
         self.file_dir = file_dir
         self.values: list = []
-        self.workbook, self.worksheet = self.load_xls_file()
+        self.contrib_data: list = []
+        (
+            self.workbook,
+            self.worksheet,
+            self.contributor_worksheet
+        ) = self.load_xls_file()
 
     def add_value(self, *args) -> None:
         for value in args:
             self.values.append(value)
+
+    def add_contrib_values(self, *args) -> None:
+        for value in args:
+            self.contrib_data.append(value)
 
     def save_file(self) -> None:
         self.workbook.save("static/METADATA-NEW-TEMPLATE-FR.xltx")
@@ -54,10 +65,11 @@ class ExcelFileHandler(ExcelUtils):
         workbook = openpyxl.load_workbook(self.file_dir)
         workbook.template = True
         worksheet = workbook["Metadatas"]
-        return workbook, worksheet
+        contributor_worksheet = workbook.worksheets[1]
+        return workbook, worksheet , contributor_worksheet
 
     def get_metadata(self, metadata: XlsMeta):
-        data: list = []
+        data, contrib_data = [], []
         data.append(metadata.track_number)
         data.append(metadata.track_title)
         data.append(metadata.subtitle)
@@ -90,7 +102,17 @@ class ExcelFileHandler(ExcelUtils):
         data.append(metadata.commercial_desc_gr)
         data.append(metadata.commercial_desc_it)
         data.append(metadata.commercial_desc_sp)
-        return data
+
+        contrib_data.append(metadata.contributor.contributor_name)
+        contrib_data.append(metadata.contributor.role1)
+        contrib_data.append(metadata.contributor.role2)
+        contrib_data.append(metadata.contributor.role3)
+        contrib_data.append(metadata.contributor.role4)
+        contrib_data.append(metadata.contributor.release_title)
+        contrib_data.append(metadata.contributor.track)
+        contrib_data.append(metadata.contributor.spotify_id)
+        contrib_data.append(metadata.contributor.apple_music_id)
+        return data, contrib_data
 
     def get_file_metadata(self, dir_path: str) -> MetaData:
         media_data: dict = dict()
@@ -101,18 +123,27 @@ class ExcelFileHandler(ExcelUtils):
     def write_in_xlsx_file(self, data: MetaData) -> None:
         xls_data = XlsMeta(song_metada=data)
         datas = self.read_xlsx_file()
-        if self.get_metadata(metadata=xls_data) not in datas:
-            metadata = [self.get_metadata(metadata=xls_data)]
+        metadatas, contrib_data = self.get_metadata(metadata=xls_data)
+        if metadatas not in datas:
+            metadata = [metadatas]
+            contributors = [contrib_data]
             self.add_value(*metadata)
+            self.add_contrib_values(*contributors)
+            self.contributor_worksheet.append(self.contrib_data[0])
             self.worksheet.append(self.values[0])
             self.values = []
+            self.contrib_data = []
         else:
             raise Exception("Already in the sheet file")
 
     def read_xlsx_file(self) -> list:
-        all_data = [list(row) for row in self.worksheet.values]
-        return all_data
+        return [list(row) for row in self.worksheet.values]
+
+
+    def read_xlsx_files(self) -> list:
+        return [list(row) for row in self.contributor_worksheet.values]
 
 
 if __name__ == "__main__":
-    pass
+    excel = ExcelFileHandler("static/METADATA-NEW-TEMPLATE-EN.xlsx")
+    print(excel.read_xlsx_files())
